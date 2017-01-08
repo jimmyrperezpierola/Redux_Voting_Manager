f6cfdc3a855370998c63da69d2fa9c8a2eca568a
bestscholar85 <marinecoder@MarineCoders-Mac.local>
2017-01-08 15:10:02 +0100

    Voting component and its test added

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
 
-const pair = ['Trainspotting', '28 Days Later'];
+const pair = ['Trainspotting', '25 Days Later'];
 
 ReactDOM.render(
 	<Voting pair={pair} />,
